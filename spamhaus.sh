1e67f75cb470a06a336a19afbfd80e9571ddebfb
onezero1010101 <onezero1010101@gmail.com>
2025-03-03 11:28:21 -0500

    Update spamhaus.sh

--- spamhaus.sh
+++ spamhaus.sh
@@ -3,3 +3,4 @@ rm drop*
 #wget https://www.spamhaus.org/drop/drop_v4.json
 jq -r 'select(.type == null) | (.cidr)' ${GITHUB_WORKSPACE}/drop_v4.json >> ${GITHUB_WORKSPACE}/dropnew.txt
 echo ${PWD}
+ls
